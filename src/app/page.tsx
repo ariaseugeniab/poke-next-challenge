71b84f103c0e0665215dc9ac35f125ac95f2bf44
ariaseugeniab <ariaseugeniab@gmail.com>
2025-06-16 22:51:03 -0300

    feat: pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,74 @@
 
 import SearchBar from '@/components/home/search-bar';
 import PokemonList from '@/components/pokemon/pokemon-list';
+import PagePaginator from '@/components/shared/pagination/page-paginator';
 import { usePokemonDetailsList, usePokemonList } from '@/hooks/use-pokemon';
 import { useQueryParams } from '@/hooks/use-query-params';
 import { QueryParamsSearchPokemonSchema } from '@/schemas/search';
+import { useCallback, useState } from 'react';
+
+const ITEMS_PER_PAGE = 27;
 
 const Page = () => {
-  const { queryParams } = useQueryParams(QueryParamsSearchPokemonSchema);
+  const { queryParams, setQueryParams } = useQueryParams(
+    QueryParamsSearchPokemonSchema
+  );
+
+  const [currentPage, setCurrentPage] = useState(
+    queryParams.page ? Number.parseInt(queryParams.page) : 1
+  );
+
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   const { data: pokemonList, isLoading: isLoadingList } = usePokemonList(
-    20,
-    0,
+    ITEMS_PER_PAGE,
+    offset,
     queryParams.name
   );
-  const pokemonNames =
-    pokemonList?.results.map((pokemon) => pokemon.name) ?? [];
 
-  console.log(pokemonList);
+  const pokemonNames =
+    pokemonList?.results
+      .filter(
+        (pokemon) =>
+          !queryParams.name ||
+          pokemon.name.toLowerCase().includes(queryParams.name.toLowerCase())
+      )
+      .map((pokemon) => pokemon.name) ?? [];
 
   const { data: pokemonDetails, isLoading: isLoadingDetails } =
     usePokemonDetailsList(pokemonNames);
 
+  const totalPages = Math.ceil((pokemonList?.count ?? 0) / ITEMS_PER_PAGE);
+
+  const handlePageChange = useCallback(
+    (page: number) => {
+      setCurrentPage(page);
+      setQueryParams({
+        ...queryParams,
+        page: page.toString(),
+      });
+    },
+    [queryParams, setQueryParams]
+  );
+
+  console.log('pokemonList', pokemonDetails);
+
   return (
-    <div>
+    <div className="space-y-4">
       <SearchBar />
 
       <PokemonList
         pokemonDetails={pokemonDetails ?? []}
         isLoading={isLoadingDetails || isLoadingList}
       />
+
+      {totalPages > 1 && (
+        <PagePaginator
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handlePageChange={handlePageChange}
+        />
+      )}
     </div>
   );
 };
