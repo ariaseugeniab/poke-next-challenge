4b576a70bde255de88b910e4b63edbd890ea365f
ariaseugeniab <ariaseugeniab@gmail.com>
2025-06-21 18:51:18 -0300

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   trailingSlash: true,
-  basePath: '/',
   reactStrictMode: false,
   images: {
     unoptimized: true,
