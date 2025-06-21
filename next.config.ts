116281aac9a9c271b094afb84ed98598a1dbc455
ariaseugeniab <ariaseugeniab@gmail.com>
2025-06-21 18:48:06 -0300

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   trailingSlash: true,
-  basePath: '/poke-next-challenge',
+  basePath: '/',
   reactStrictMode: false,
   images: {
     unoptimized: true,
