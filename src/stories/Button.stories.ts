a9f79bd4330ea55d96b3225ae951820140bab33a
eugenia arias <ariaseugeniab@gmail.com>
2025-07-28 15:22:03 -0300

    feat:add storybook

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+import { fn } from 'storybook/test';
+
+import { Button } from '@/components/shared/button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Button',
+  component: Button,
+  parameters: {
+    className: 'w-full',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    variant: 'default',
+    size: 'default',
+    children: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    size: 'default',
+    children: 'Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    variant: 'default',
+    size: 'lg',
+    children: 'Button',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    variant: 'default',
+    size: 'sm',
+    children: 'Button',
+  },
+};
